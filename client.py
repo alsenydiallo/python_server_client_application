5d99b725aa5a80ec7e44766d1f3180d41253d6dc
alsenydiallo <alsenidiallo@gmail.com>
2019-03-20 11:43:54 -0700

    fixed bug at sending location between peer

--- client.py
+++ client.py
@@ -196,8 +196,8 @@ def add_to_dic(address, location):
 
 def peers_locations_list():
     locations = []
-    for peer in list_of_clients:
-        locations.append(list_of_clients[peer])
+    for peer in peers_locations:
+        locations.append(peers_locations[peer])
     return locations
 
 
